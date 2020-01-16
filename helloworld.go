cead90164b72995b6cf46d5468e7ce46b26922aa
hongtao.wan <hongtao.wan@aispeech.com>
2020-01-16 10:36:40 +0800

    增加golang笔记

--- helloworld.go
+++ helloworld.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"reflect"
 	"runtime"
+	"sort"
 	"strconv"
 	"sync"
 	"time"
@@ -76,6 +77,8 @@ func main() {
 
 	slinceTest()
 
+	sortTest()
+
 	chanTimeout()
 
 	checkChannelIsFull()
@@ -93,6 +96,7 @@ func main() {
 	lockTest()
 
 	runnerTest()
+
 }
 
 func funtest() {
@@ -679,4 +683,40 @@ func createTask() func(int) {
 	}
 }
 
+// 将[]string定义为MyStringList类型
+type MyStringList []string
 
+// 实现sort.Interface接口的获取元素数量方法
+func (m MyStringList) Len() int  {
+	return len(m)
+}
+
+// 实现sort.Interface接口的比较元素方法
+func (m MyStringList) Less(i,j int) bool {
+	return m[i] < m[j]
+}
+
+// 实现sort.Interface接口的交换元素方法
+func (m MyStringList) Swap(i,j int)  {
+	m[i],m[j] = m[j],m[i]
+}
+
+func sortTest()  {
+	// 准备一个内容被打乱顺序的字符串切片
+	names := MyStringList{
+		"3. Triple Kill",
+		"5. Penta Kill",
+		"2. Double Kill",
+		"4. Quadra Kill",
+		"1. First Blood",
+	}
+
+	// 使用sort包进行排序
+	sort.Sort(names)
+	// 遍历打印结果
+	for _, v := range names {
+		log.Println(v)
+	}
+
+	printLine()
+}
